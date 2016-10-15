70938686d53800b95aecbc84f00be10ee3fa5e15
W.B. Metzel <w.metzel@gmail.com>
2016-10-15 13:06:56 -0400

    Comment added to batch file

--- csc.bat
+++ csc.bat
@@ -1,3 +1,7 @@
+:: To use this script to compile csharp programs:
+:: 1. Edit file path to the csc.exe releveant to your local machine
+:: 2. With cmd, type:  csc filename.cs
+
 @echo CSharp Command-Line Compile
 @echo off
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe -nologo %1 %2 %3 %4 %5 %6 %7 %8
